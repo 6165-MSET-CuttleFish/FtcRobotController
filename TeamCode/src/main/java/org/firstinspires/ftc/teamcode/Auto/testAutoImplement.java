b11e97cce708e14232c372a6bf221c821ec89b68
ayuram <54257595+ayuram@users.noreply.github.com>
2020-11-22 14:52:42 -0800

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/testAutoImplement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/testAutoImplement.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.Auto;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -8,9 +10,25 @@ import org.firstinspires.ftc.teamcode.Components.Robot;
 
 @Autonomous
 public class testAutoImplement extends LinearOpMode{
-    Robot robot;
+    //Robot robot;
+    BNO055IMU imu;
     public void runOpMode() throws InterruptedException{
-        robot = new Robot(DcMotor.RunMode.RUN_USING_ENCODER, hardwareMap, 0, 0, 18, 18);
+        //robot = new Robot(DcMotor.RunMode.RUN_USING_ENCODER, hardwareMap, 0, 0, 18, 18);
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
 
+        //Initialize IMU parameters
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        imu.initialize(parameters);
+        while(opModeIsActive()){
+            telemetry.addData("first",imu.getAngularOrientation().firstAngle);
+            telemetry.addData("second",imu.getAngularOrientation().secondAngle);
+            telemetry.addData("third",imu.getAngularOrientation().thirdAngle);
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry/GlobalCoordinatePositionUpdateSample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry/GlobalCoordinatePositionUpdateSample.java
@@ -16,7 +16,7 @@ public class GlobalCoordinatePositionUpdateSample extends LinearOpMode {
 
     //The amount of encoder ticks for each inch the robot moves. This will change for each robot and needs to be changed here
     //final double COUNTS_PER_INCH = 307.699557;
-    final double COUNTS_PER_INCH = 560;
+    final double COUNTS_PER_INCH = 768;
 
     //Hardware map names for the encoder wheels. Again, these will change for each robot and need to be updated below
     String verticalLeftEncoderName = "frontLeft", verticalRightEncoderName = "frontRight", horizontalEncoderName = "backRight";
