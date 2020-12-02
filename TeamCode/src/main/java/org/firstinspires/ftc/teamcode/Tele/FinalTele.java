ad7815cbb30053833c521132708e4c6bc49fdf3c
MatthewSong86 <54823079+MatthewSong86@users.noreply.github.com>
2020-12-01 16:02:51 -0800

    Update FinalTele.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/FinalTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/FinalTele.java
@@ -220,7 +220,7 @@ public class FinalTele extends LinearOpMode implements Runnable{
             mag.setPosition(.47);
         }
         if(gamepad2.left_trigger >=0.1){
-            tilt.setPosition(0.31);
+            tilt.setPosition(0.305);
             flywheel.setPower(-1);
             flywheel1.setPower(-1);
         }
