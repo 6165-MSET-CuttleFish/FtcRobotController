ba7ea15fc29f06baea688f5794c418e6ea18ea02
MatthewSong86 <54823079+MatthewSong86@users.noreply.github.com>
2020-12-03 16:39:48 -0800

    Update FinalTele.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/FinalTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/FinalTele.java
@@ -155,19 +155,17 @@ public class FinalTele extends LinearOpMode{
         br.setPower(v4);
     }
     public void wobbleArm(){
-        if(gamepad2.a == true && !armUp && lastTime > System.currentTimeMillis() + 300){
+        if(gamepad2.a == true && armUp == false && lastTime > System.currentTimeMillis() + 300){
             arm1.setPosition(0.92);
             arm2.setPosition (0.92);
             armUp = true;
-            //sleep(100);
             lastTime = System.currentTimeMillis();
         }
         else if(gamepad2.a == true && armUp == true && lastTime > System.currentTimeMillis() + 300){
-            //0.07 init
+
             arm1.setPosition(0.13);
             arm2.setPosition (0.13);
             armUp = false;
-            //sleep(100);
             lastTime = System.currentTimeMillis();
         }
 
