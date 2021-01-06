0e38e05171cb94689180a478eb557f6da5398b2a
ayuram <54257595+ayuram@users.noreply.github.com>
2021-01-05 18:46:07 -0800

    Update singleDriver.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/singleDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/singleDriver.java
@@ -45,6 +45,7 @@ public class singleDriver extends LinearOpMode {
         //position = new OdometryGlobalCoordinatePosition(br, bl, fr, COUNTS_PER_INCH, 75, 0, 0);
         Robot robot = new Robot(DcMotor.RunMode.RUN_WITHOUT_ENCODER, hardwareMap, 0, 0, 0, 18, 18);
         Thread positionThread = new Thread(position);
+        pidRotate = new PIDController(.07, 0.013, 0.0044);
         positionThread.start();
         shootingPos = new Coordinate(position);
         shootingAngle = position.radians();
