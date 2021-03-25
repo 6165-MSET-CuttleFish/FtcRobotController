a97fd7ba5543c56fd74d40a111fad73d19d98826
ayuram <54257595+ayuram@users.noreply.github.com>
2021-03-24 18:44:13 -0700

    Update StandardTrackingWheelLocalizer.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -31,11 +31,11 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 1.5/2;// in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 13.2285; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = -4.8;//0.26; // in; offset of the lateral wheel
+    public static double LATERAL_DISTANCE = 13.2452; // in; distance between the left and right wheels
+    public static double FORWARD_OFFSET = -5.15;//0.26; // in; offset of the lateral wheel
 
     public static double X_MULTIPLIER = 1; // Multiplier in the X direction
-    public static double Y_MULTIPLIER = 0.96920898; // Multiplier in the Y direction
+    public static double Y_MULTIPLIER = 0.97440337; // Multiplier in the Y direction
 
     public Encoder leftEncoder, rightEncoder, frontEncoder;
 
