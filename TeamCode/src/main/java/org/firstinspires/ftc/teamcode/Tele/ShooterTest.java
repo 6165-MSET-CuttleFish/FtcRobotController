ccd1a27f25ed3f131b43851e466c199e080eb942
ayuram <54257595+ayuram@users.noreply.github.com>
2021-03-30 10:36:03 -0700

    Update ShooterTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/ShooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/ShooterTest.java
@@ -26,29 +26,41 @@ import static org.firstinspires.ftc.teamcode.PurePursuit.MathFunctions.AngleWrap
 public class ShooterTest extends LinearOpMode
 {
     Robot robot;
-    Runnable wingDefault;
-    boolean ninja, armUp;
-    Coordinate shootingPosition;
-    double shootingAngle = 0;
-    boolean isWingsOut = false;
-    double velo = 0;
-    int wingsLimit = 0;
-    ArrayList<Double> timer = new ArrayList<Double> ();
-    double currentMillis = 0;
-    double lastMillis = 0;
-    int cycles = 0;
     @Override
     public void runOpMode() throws InterruptedException {
 
-        //robot = new Robot (hardwareMap, telemetry, () -> opModeIsActive() && gamepadIdle());
+        robot = new Robot (hardwareMap, telemetry, this::opModeIsActive);
 
        // robot = new Robot (hardwareMap, telemetry, () -> opModeIsActive() && gamepadIdle());
-
+        double targetVelo = 1000;
         robot.init();
         //wingDefault = ()->robot.launcher.wingsVert();
         // pidRotate = new PIDController(.07, 0.014, 0.0044);
-        //Thread driveTrain = new Thread(this);
+        Thread shooterThread = new Thread(()->{
+            while(opModeIsActive()){
+                robot.launcher.updatePID();
+            }
+        });
         waitForStart();
+        shooterThread.start();
+        while(opModeIsActive()){
+            if(gamepad1.dpad_up){
+                targetVelo += 20;
+                sleep(300);
+            } else if(gamepad1.dpad_down){
+                targetVelo -= 10;
+                sleep(300);
+            }
+            if(gamepad1.left_trigger >= 0.1) {
+                robot.launcher.setVelocity(targetVelo);
+            } else {
+                robot.launcher.setVelocity(0);
+                if (gamepad1.left_bumper) robot.launcher.tiltUp();
+                else robot.launcher.tiltDown();
+            }
+            telemetry.addData("targetVelo ", targetVelo);
+            telemetry.addData("current velo ", robot.launcher.getVelocity());
+        }
     }
 
 }
