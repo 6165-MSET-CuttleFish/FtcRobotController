725e460fdc9a06676006da83d2c32ecf970b1e3b
ayuram <54257595+ayuram@users.noreply.github.com>
2021-06-20 15:58:52 -0700

    Revert "turret testing"
    This reverts commit ef1b3eaca476acde00d18e2d341dd293449d7a84.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/TurretTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/TurretTest.java
@@ -1,8 +1,6 @@
 package org.firstinspires.ftc.teamcode.Tele;
 
-import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -12,19 +10,15 @@ import org.firstinspires.ftc.teamcode.Components.Turret;
 @TeleOp(name = "TurretTest", group = "LinearOpMode")
 public class TurretTest extends LinearOpMode {
     Turret turret;
-    FtcDashboard dashboard = FtcDashboard.getInstance();
 
+    private double angle = 1;
     public void runOpMode() throws InterruptedException {
         turret = new Turret(hardwareMap);
-        double angle = 45;
-        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
         waitForStart();
         while (opModeIsActive()){
-            turret.setTargetAngle(Math.toRadians(angle));
+            turret.setTargetAngle(angle+60);
             turret.update();
-            telemetry.addData("Current Angle", Math.toDegrees(turret.getAbsoluteAngle()));
-            telemetry.addData("target angle", angle);
-            telemetry.update();
+
         }
     }
 }
