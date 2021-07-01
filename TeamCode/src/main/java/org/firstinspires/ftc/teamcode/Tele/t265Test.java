34e8c0d8cedf7c73b178c64977990b6b2737b77f
ayuram <54257595+ayuram@users.noreply.github.com>
2021-06-30 18:34:31 -0700

    Update t265Test.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/t265Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/t265Test.java
@@ -1,67 +1,20 @@
 package org.firstinspires.ftc.teamcode.Tele;
 
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.canvas.Canvas;
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.arcrobotics.ftclib.geometry.Rotation2d;
-import com.arcrobotics.ftclib.geometry.Transform2d;
-import com.arcrobotics.ftclib.geometry.Translation2d;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.spartronics4915.lib.T265Camera;
+
+import org.firstinspires.ftc.teamcode.Components.localizer.IntelLocalizer;
 
 @TeleOp
-public class t265Test extends OpMode {
-    private static T265Camera slamra = null;
-
-    private final FtcDashboard dashboard = FtcDashboard.getInstance();
-
+public class t265Test extends LinearOpMode {
+    IntelLocalizer localizer;
     @Override
-    public void init() {
-        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
-        if (slamra == null) {
-            slamra = new T265Camera(new Transform2d(), 0.1, hardwareMap.appContext);
+    public void runOpMode() throws InterruptedException {
+        localizer = new IntelLocalizer(hardwareMap, new Pose2d(0, 0, 0));
+        waitForStart();
+        while(opModeIsActive()) {
+            localizer.update();
         }
-    }
-
-    @Override
-    public void init_loop() {
-    }
-
-    @Override
-    public void start() {
-        slamra.start();
-    }
-
-    @Override
-    public void loop() {
-        final int robotRadius = 9; // inches
-
-        TelemetryPacket packet = new TelemetryPacket();
-        Canvas field = packet.fieldOverlay();
-
-        T265Camera.CameraUpdate up = slamra.getLastReceivedCameraUpdate();
-        if (up == null) return;
-
-        // We divide by 0.0254 to convert meters to inches
-        Translation2d translation = new Translation2d(up.pose.getTranslation().getX() / 0.0254, up.pose.getTranslation().getY() / 0.0254);
-        Rotation2d rotation = up.pose.getRotation();
-
-        field.strokeCircle(translation.getX(), translation.getY(), robotRadius);
-        double arrowX = rotation.getCos() * robotRadius, arrowY = rotation.getSin() * robotRadius;
-        double x1 = translation.getX() + arrowX  / 2, y1 = translation.getY() + arrowY / 2;
-        double x2 = translation.getX() + arrowX, y2 = translation.getY() + arrowY;
-        field.strokeLine(x1, y1, x2, y2);
-        telemetry.addData("x", translation.getX());
-        telemetry.addData("y", translation.getY());
-        telemetry.update();
-
-        dashboard.sendTelemetryPacket(packet);
-    }
-
-    @Override
-    public void stop() {
-        slamra.stop();
     }
 }
