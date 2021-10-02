b9f8b32046746a9109b981fef0156bd393af0cf4
Ayush Raman <54257595+ayuram@users.noreply.github.com>
2021-10-01 21:14:37 -0700

    Update LocalizationTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/LocalizationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/LocalizationTest.java
@@ -29,7 +29,7 @@ public class LocalizationTest extends LinearOpMode {
             robot.setWeightedDrivePower(
                     new Pose2d(
                             -gamepad1.left_stick_y,
-                            -gamepad1.left_stick_x,
+                            0,
                             -gamepad1.right_stick_x
                     )
             );
