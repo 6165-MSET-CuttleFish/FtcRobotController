83f4a18247a3eb407a39c6ce083d097729bc2ac7
billofright <bill.yuan27@gmail.com>
2021-10-09 16:07:53 -0700

    vision boiler code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/vision/detector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/vision/detector.java
@@ -8,7 +8,7 @@ import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
-
+//
 public class detector extends OpenCvPipeline {
     Telemetry telemetry;
     Mat mat = new Mat();
