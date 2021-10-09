591cad26bddb883235b55ed7d4da7e9d18e25e1e
Ayush Raman <54257595+ayuram@users.noreply.github.com>
2021-10-08 19:59:25 -0700

    fixed?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Robot.java
@@ -356,6 +356,12 @@ public class Robot extends TankDrive {
     public void setWeightedDrivePower(Pose2d drivePower) {
         Pose2d vel = drivePower;
 
+        vel = new Pose2d(
+                drivePower.getX(),
+                drivePower.getY(),
+                drivePower.getHeading() * 2
+        );
+
         if (Math.abs(drivePower.getX()) + Math.abs(drivePower.getHeading()) > 1) {
             // re-normalize the powers according to the weights
             double denom = VX_WEIGHT * Math.abs(drivePower.getX())
