b57c6c2f945b57e0092b7b80f79e9ed0f8fb44b7
ayuram <54257595+ayuram@users.noreply.github.com>
2021-11-24 11:38:54 -0800

    Update LimitSwitch.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/wrappers/LimitSwitch.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/wrappers/LimitSwitch.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.modules;
+package org.firstinspires.ftc.teamcode.modules.wrappers;
 
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
