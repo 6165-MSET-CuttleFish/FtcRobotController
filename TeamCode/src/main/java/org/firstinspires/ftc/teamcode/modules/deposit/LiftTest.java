77778eaa0a28bef222f2655492e1bdf76dd763e9
Ayush Raman <54257595+ayuram@users.noreply.github.com>
2021-11-26 15:55:16 -0800

    Create LiftTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/deposit/LiftTest.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.modules.deposit;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.modules.intake.Intake;
+import org.firstinspires.ftc.teamcode.util.opmode.ModuleTest;
+import org.firstinspires.ftc.teamcode.util.tuning.TuningController;
+
+@TeleOp
+public class LiftTest extends ModuleTest {
+    Deposit deposit;
+    TuningController<Deposit.State> tuningController = new TuningController<>(Deposit.State.values(), 1);
+    @Override
+    public void init() {
+        deposit = new Deposit(hardwareMap, new Intake(hardwareMap));
+        Platform.isLoaded = true;
+        init(deposit);
+    }
+
+    @Override
+    public void loop() {
+        update();
+        deposit.setState(tuningController.update());
+    }
+}
