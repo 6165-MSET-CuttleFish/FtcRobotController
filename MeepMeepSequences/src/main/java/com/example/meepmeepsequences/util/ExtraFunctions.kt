bacf8f03a646f105930d7120b2f1481a66bb9dcd
Ayush Raman <54257595+ayuram@users.noreply.github.com>
2021-11-26 23:56:25 -0800

    Update ExtraFunctions.kt

--- MeepMeepSequences/src/main/java/com/example/meepmeepsequences/util/ExtraFunctions.kt
+++ MeepMeepSequences/src/main/java/com/example/meepmeepsequences/util/ExtraFunctions.kt
@@ -65,7 +65,7 @@ fun MeepMeep.configure(): MeepMeep {
         .setDriveTrainType(DriveTrainType.TANK)
         .setDarkMode(true)
         .setBotDimensions(17.2, 17.192913)
-        .setConstraints(74.76041056423146, 74.76041056423146, Math.toRadians(274.5043079608481), Math.toRadians(274.5043079608481), 15.0)
+        .setConstraints(60.0, 60.0, Math.toRadians(274.5043079608481), Math.toRadians(274.5043079608481), 15.0)
 }
 
 interface FutureCallback {
