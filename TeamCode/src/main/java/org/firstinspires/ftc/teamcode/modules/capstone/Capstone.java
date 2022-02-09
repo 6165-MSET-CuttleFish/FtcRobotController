59e218e9f225672158530be019302417438e5323
Ayush Raman <54257595+ayuram@users.noreply.github.com>
2022-02-09 09:54:23 -0800

    capstone

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/capstone/Capstone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/capstone/Capstone.java
@@ -4,7 +4,6 @@ import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.modules.Module;
@@ -14,7 +13,7 @@ import org.firstinspires.ftc.teamcode.modules.wrappers.ControllableServos;
 @Config
 public class Capstone extends Module<Capstone.State> {
     public static double servoIncrementHorizontal = 0.007, servoIncrementVertical = 0.001;
-    public static double horizontalTolerance = 0.2, verticalTolerance = 0.2;
+    public static double horizontalTolerance = 0, verticalTolerance = 0;
     public static double servoIncrementHorizontalLarge = 0.01, servoIncrementVerticalLarge = 0.01;
     private double horizontalPos = 0.5, verticalPos = 0.8;
     private CRServo tape;
@@ -26,11 +25,9 @@ public class Capstone extends Module<Capstone.State> {
     }
 
     public enum State implements StateBuilder {
-        READY,
-        PICKING_UP,
-        HOLDING,
-        PRECAP,
-        CAPPING;
+        IDLE,
+        AUTORETRACT,
+        ;
 
         @Override
         public Double getTimeOut() {
@@ -42,7 +39,7 @@ public class Capstone extends Module<Capstone.State> {
      * @param hardwareMap  instance of the hardware map provided by the OpMode
      */
     public Capstone(HardwareMap hardwareMap) {
-        super(hardwareMap, State.HOLDING);
+        super(hardwareMap, State.IDLE);
     }
 
     public void internalInit() {
@@ -52,34 +49,27 @@ public class Capstone extends Module<Capstone.State> {
         verticalTurret = new ControllableServos(hardwareMap.servo.get("vTurret"));
         setActuators(horizontalTurret, verticalTurret);
     }
-    public double getVerticalTurretPos(){
-        return verticalTurret.getPosition();
-    }
+
     @Override
     protected void internalUpdate() {
         verticalPos = Range.clip(verticalPos, 0, 1);
         horizontalPos = Range.clip(horizontalPos, 0, 1);
+        verticalTurret.setPosition(verticalPos);
+        horizontalTurret.setPosition(horizontalPos);
     }
 
     public void setHorizontalTurret(double pwr) {
-        if (pwr > horizontalTolerance) horizontalPos += servoIncrementHorizontal;
-        else if (pwr < -horizontalTolerance) horizontalPos -= servoIncrementHorizontal;
-        horizontalTurret.setPosition(horizontalPos);
+        if (Math.abs(pwr) > horizontalTolerance) horizontalPos += servoIncrementHorizontal * pwr;
+
     }
     public void incrementHorizontal(double pwr) {
-        if (pwr > horizontalTolerance) horizontalPos += servoIncrementHorizontalLarge;
-        else if (pwr < -horizontalTolerance) horizontalPos -= servoIncrementHorizontalLarge;
-        horizontalTurret.setPosition(horizontalPos);
+        if (Math.abs(pwr) > horizontalTolerance) horizontalPos += servoIncrementHorizontalLarge * pwr;
     }
     public void incrementVertical(double pwr) {
-        if (pwr > -verticalTolerance) verticalPos += servoIncrementVerticalLarge;
-        else if (pwr < verticalTolerance) verticalPos -= servoIncrementVerticalLarge;
-        verticalTurret.setPosition(verticalPos);
+        if (Math.abs(pwr) > verticalTolerance) verticalPos += servoIncrementVerticalLarge * pwr;
     }
     public void setVerticalTurret(double pwr) {
-        if (pwr < -verticalTolerance) verticalPos += servoIncrementVertical;
-        else if (pwr > verticalTolerance) verticalPos -= servoIncrementVertical;
-        verticalTurret.setPosition(verticalPos);
+        if (Math.abs(pwr) > verticalTolerance) verticalPos += servoIncrementVertical * pwr;
     }
     public void setTape(double pwr) {
         tape.setPower(pwr);
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/wrappers/ControllableCRServo.kt
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.modules.wrappers
+
+import com.qualcomm.robotcore.hardware.CRServo
+import com.qualcomm.robotcore.util.ElapsedTime
+
+class ControllableCRServo(private val crServo: CRServo) {
+    private var internalPosEstimate = 0.0
+    private var timer = ElapsedTime()
+    var isRetracting = false
+    var power: Double
+        get() = crServo.power
+        set(val1) {
+            crServo.power = val1
+        }
+    fun update() {
+        internalPosEstimate += power * timer.seconds()
+        timer.reset()
+        if (isRetracting) {
+            if (internalPosEstimate <= 0) {
+                power = 0.0
+                isRetracting = false
+            } else {
+                power = -1.0
+            }
+        }
+    }
+}
