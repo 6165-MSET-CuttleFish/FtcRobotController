5fb5edb198f9bdd035c39f7b20e8f1011a1a557e
ayuram <54257595+ayuram@users.noreply.github.com>
2022-04-11 00:24:46 -0700

    Update SeriesServoTuner.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/tuning/SeriesServoTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/tuning/SeriesServoTuner.java
@@ -10,6 +10,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.modules.wrappers.actuators.ControllableServos;
 import org.firstinspires.ftc.teamcode.util.Encoder;
+import org.firstinspires.ftc.teamcode.util.field.Context;
 
 import java.util.ArrayList;
 
@@ -19,26 +20,44 @@ public class SeriesServoTuner extends LinearOpMode {
     public static String servoNames;
     public static String encoder;
     public static double position;
+    public static boolean calibrate;
+
+    private String lastServoNames = servoNames;
+    private String lastEncoder = encoder;
+
+    private ControllableServos servo;
+    FtcDashboard ftcDashboard = FtcDashboard.getInstance();
+
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         waitForStart();
         while (opModeIsActive()) {
-            try {
-
-                String[] names = servoNames.split(",");
-                Servo[] servos = new Servo[names.length];
-                for (int i = 0; i < names.length; i++) {
-                    servos[i] = hardwareMap.servo.get(names[i].trim());
+            if (!lastEncoder.equals(encoder) || !lastServoNames.equals(servoNames)) {
+                lastEncoder = encoder;
+                lastServoNames = servoNames;
+                try {
+                    String[] names = servoNames.split(",");
+                    Servo[] servos = new Servo[names.length];
+                    for (int i = 0; i < names.length; i++) {
+                        servos[i] = hardwareMap.servo.get(names[i].trim());
+                    }
+                    servo = new ControllableServos(servos);
+                    if (!encoder.isEmpty()) {
+                        servo.setEncoder(new Encoder(hardwareMap.get(DcMotorEx.class, encoder)));
+                    }
+                } catch(Exception exception) {
+                    telemetry.addData("Error", exception.getMessage());
                 }
-                ControllableServos controllableServos = new ControllableServos(servos);
-                if (!encoder.isEmpty()) {
-                    controllableServos.setEncoder(new Encoder(hardwareMap.get(DcMotorEx.class, encoder)));
-                }
-                controllableServos.setPosition(position);
-            } catch(Exception exception) {
-                telemetry.addData("Error", exception.getMessage());
             }
+            servo.setPosition(position);
+            if (calibrate) {
+                servo.init(servo.getPosition());
+                calibrate = false;
+            }
+            Context.packet.put("Target Position", servo.getPosition());
+            Context.packet.put("Estimated Position", servo.getEstimatedPosition());
+            Context.packet.put("Real Position", servo.getRealPosition());
             telemetry.update();
         }
     }
